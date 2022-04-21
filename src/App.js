11169b290af05e110e022faedce349fdbd53fe9e
Austin <ofthevalley111@gmail.com>
2022-04-21 11:35:55 -0500

    Completed and instantiated AddEntryForm component in App component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
-
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
 
 
 
 function App() {
 
-    const [entries, setEntries] = useState([{name : "JJ V.", post: "I have a sense of humor!"  }, {name : "Megan C.", post: "Kirby is where it is at!"  }])
+    const [entries, setEntries] = useState([{name : "JJ V.", post: "I have a sense of humor!"  }, {name : "Megan C.", post: "Kirby is the best!"  }])
 
 
+
+    function addNewEntry(entry){
+        
+        let tempEntries = [entry,...entries];
+        
+        setEntries(tempEntries);
+
+
+    }
     return (
     <div>
         <DisplayEntries parentEntries={entries}/>
+        <AddEntryForm AddNewEntryProperty={addNewEntry}/>
 
     </div>
 )
---
+++ src/Components/AddEntry/AddEntryForm.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+
+
+
+const AddEntryForm = (props) => {
+    
+    
+    const [name, setName] = useState('')
+    const [post, setPost] = useState('')
+    
+    function handleSubmit(event){
+        event.preventDefault();
+        let newEntry = {
+            name : name,
+            post: post
+
+        }
+        console.log(newEntry)
+        props.AddNewEntryProperty(newEntry)
+    }
+
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>Name</label>
+            <input type='text' value = {name} onChange={(event) => {setName(event.target.value)}}/>
+            <label>Post</label>
+            <input type='text' value = {post} onChange={(event) => {setPost(event.target.value)}}/>
+            <button type = 'submit'>Post</button>
+        </form>
+      );
+}
+ 
+export default AddEntryForm;
